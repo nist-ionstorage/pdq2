8ae6337dedcf749c5cfbd65f6d72017c17fa5316
Robert Jordens <jordens@phys.ethz.ch>
2013-06-02 17:36:04 -0600

    cmd fsm

--- comm.py
+++ comm.py
@@ -78,9 +78,66 @@ class Parser(Module):
                 we_granularity=8) for dac in dacs]
         self.specials += mems
 
-        states = "CMD DATA".split()
+        states = "CMD ARG1 ARG2 DATA PARSE".split()
         fsm = FSM(*states)
         self.submodules += fsm
+
+        cmds = ("DEV_ADDR DATA_LENGTH MEM_ADDR SINGLE BURST _5 MEM_LENGTH "
+            "_7 MODE _9 DAC0 DAC1 DAC2 _13 _14 _15 _16 _17 _18 _19 "
+            "BRANCH0 BRANCH1 BRANCH2 BRANCH3 BRANCH4 BRANCH5 BRANCH6").split()
+        assert len(cmds) == 27, len(cmds)
+        code_to_cmd = dict((code, cmd) for code, cmd in enumerate(cmds))
+
+        cmd = Signal(8)
+        arg1 = Signal(8)
+        arg2 = Signal(8)
+        length = Signal(16)
+        read = Signal(16)
+        
+        pd = self.data_in.payload.data 
+
+        actions = {}
+
+        fsm.act(fsm.CMD,
+                self.data_in.ack.eq(1),
+                If(self.data_in.stb,
+                    cmd.eq(pd),
+                    self.data_in.ack.eq(0),
+                    If((pd == cmds.index("BURST")) |
+                       (pd == cmds.index("SINGLE")),
+                        fsm.next_state(fsm.DATA),
+                    ).Else(
+                        fsm.next_state(fsm.ARG1),
+                    ),
+                ))
+        fsm.act(fsm.ARG1,
+                self.data_in.ack.eq(1),
+                If(self.data_in.stb,
+                    self.data_in.ack.eq(0),
+                    arg1.eq(pd),
+                    fsm.next_state(fsm.ARG2),
+                ))
+        fsm.act(fsm.ARG2,
+                self.data_in.ack.eq(1),
+                If(self.data_in.stb,
+                    arg2.eq(pd),
+                    self.data_in.ack.eq(0),
+                    fsm.next_state(fsm.CMD),
+                    Case(cmd, actions),
+                ))
+        fsm.act(fsm.DATA,
+                self.data_in.ack.eq(1),
+                If(self.data_in.stb,
+                    If(read < length,
+                        read.eq(read + 1),
+                        fsm.next_state(fsm.DATA),
+                    ).Else(
+                        read.eq(0),
+                        fsm.next_state(fsm.CMD),
+                    ),
+                    Case(cmd, actions),
+                    self.data_in.ack.eq(0),
+                ))
 
 
 
