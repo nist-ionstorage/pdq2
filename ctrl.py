d7770212bf043cb583daafd3f5fc2e6f3502915d
Robert Jordens <jordens@phys.ethz.ch>
2013-06-02 23:24:25 -0600

    make branch and trigger sync

--- ctrl.py
+++ ctrl.py
@@ -2,6 +2,6 @@ from migen.fhdl.std import *
 
 class Ctrl(Module):
     def __init__(self, pads, *dacs):
-        self.comb += [dac.reader.branch.eq(pads.branch) for dac in dacs]
-        self.comb += [dac.out.trigger.eq(pads.trigger) for dac in dacs]
+        self.sync += [dac.reader.branch.eq(pads.branch) for dac in dacs]
+        self.sync += [dac.out.trigger.eq(pads.trigger) for dac in dacs]
         self.comb += pads.aux.eq(0)
