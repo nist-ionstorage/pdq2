739ccc6fd680e6b0dd7b54b0dd264dda9731a900
Robert Jordens <jordens@phys.ethz.ch>
2013-06-05 12:22:09 -0600

    simplify dac

--- dac.py
+++ dac.py
@@ -1,9 +1,7 @@
 from migen.fhdl.std import *
-from migen.genlib.fsm import FSM
 from migen.genlib.record import Record
-from migen.flow.actor import *
-from migen.flow.network import *
-from migen.actorlib import sim
+from migen.flow.actor import Source, Sink
+from migen.flow.network import CompositeActor, DataFlowGraph
 
 
 frame_layout = [
@@ -18,12 +16,10 @@ frame_layout = [
 
 class DacReader(Module):
     def __init__(self, mem_data_width=16, mem_adr_width=13,
-            mem_depth=6*(1<<10)): # XC3S500E: 20BRAMS 18bits 1024loc
+            mem_depth=4*(1<<10)): # XC3S500E: 20BRAMS 18bits 1024loc
         self.specials.mem = Memory(width=mem_data_width, depth=mem_depth)
         read = self.mem.get_port()
         self.specials.read = read
-        cur_adr = Signal(mem_adr_width)
-        self.sync += cur_adr.eq(read.adr)
         self.order = Signal(2)
         self.branch = Signal(3)
         self.branch_adrs = Array(Signal(mem_adr_width) for _ in range(8))
@@ -39,129 +35,85 @@ class DacReader(Module):
                 ]
 
         self.frame_out = Source(frame_layout)
-        fp_send = self.frame_out.payload
-        fp = Record(frame_layout)
-        self.sync += fp_send.raw_bits().eq(fp.raw_bits())
+        fp = self.frame_out.payload
         self.busy = ~self.frame_out.stb
 
-        states = "INIT V0 DT V1A V1B V2A V2B V2C V3A V3B V3C IDLE".split()
-        fsm = FSM(*states)
-        self.submodules.fsm = fsm
+        states = dict((v, i) for i, v in enumerate(
+            "IDLE V0 DT V1A V1B V2A V2B V2C V3A V3B V3C".split()))
+        state = Signal(max=len(states))
+        self.state = state
 
-        read_states = states[1:]
-        for i, state in enumerate(read_states[:-1]):
-            fsm.act(getattr(fsm, state),
-                read.adr.eq(cur_adr + 1),
-                fsm.next_state(getattr(fsm, read_states[i+1])),
-                fp.raw_bits().eq(fp_send.raw_bits()),
-                )
-        
-        fsm.act(fsm.INIT,
-                If((cur_adr < branch_start) | 
-                   (cur_adr >= branch_end),
-                    read.adr.eq(branch_start),
-                ).Else(
-                    read.adr.eq(cur_adr),
-                ),
-                fsm.next_state(fsm.V0),
-                )
-        fsm.act(fsm.V0,
-                fp.v0[32:].eq(read.dat_r[:16]),
-                If(self.order == 0,
-                    fp.wait.eq(0),
-                    fp.dt.eq(3), # INIT, V0, IDLE
-                    fp.v1.eq(0),
-                    fp.v2.eq(0),
-                    fp.v3.eq(0),
-                    fsm.next_state(fsm.IDLE),
-                ))
-        fsm.act(fsm.DT,
-                fp.wait.eq(read.dat_r[15]),
-                If(read.dat_r[15],
-                    fp.dt.eq(1 + ~read.dat_r[:15]),
-                ).Else(
-                    fp.dt.eq(read.dat_r[:15]),
-                ),
-                If(self.order == 1,
-                    fp.v1.eq(0),
-                    fp.v2.eq(0),
-                    fp.v3.eq(0),
-                    fsm.next_state(fsm.IDLE),
-                ))
-        fsm.act(fsm.V1A,
-                fp.v1[16:32].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V1B,
-                fp.v1[32:].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V2A,
-                fp.v2[:16].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V2B,
-                fp.v2[16:32].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V2C,
-                fp.v2[32:].eq(read.dat_r[:16]),
-                If(self.order == 2,
-                    fp.v3.eq(0),
-                    fsm.next_state(fsm.IDLE),
-                ))
-        fsm.act(fsm.V3A,
-                fp.v3[:16].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V3B,
-                fp.v3[16:32].eq(read.dat_r[:16]),
-                )
-        fsm.act(fsm.V3C,
-                fp.v3[32:].eq(read.dat_r[:16]),
-                If(self.order == 3,
-                    fsm.next_state(fsm.IDLE),
-                ),
-                )
-        fsm.act(fsm.IDLE,
-                read.adr.eq(cur_adr),
-                self.frame_out.stb.eq(1),
-                fp.raw_bits().eq(fp_send.raw_bits()),
-                If(self.frame_out.ack,
-                    fsm.next_state(fsm.INIT),
-                ),
-                )
+        read_next = lambda reg, s: [
+                reg.eq(read.dat_r[:16]),
+                read.adr.eq(read.adr + 1),
+                state.eq(states[s]),
+                ]
+        short_send = lambda order: [
+                If(self.order == order,
+                    self.frame_out.stb.eq(1),
+                    state.eq(states["IDLE"]),
+                )]
 
+        actions = {
+                "IDLE": [
+                    If(self.frame_out.ack | ~self.frame_out.stb,
+                        self.frame_out.stb.eq(0),
+                        fp.eq(0),
+                        read.adr.eq(read.adr + 1),
+                        state.eq(states["V0"]),
+                    )],
+                "V0": read_next(fp.v0[32:], "DT") + short_send(0),
+                "DT": read_next(fp.dt, "V1A") + short_send(1) + [
+                    fp.wait.eq(read.dat_r[15]),
+                    If(read.dat_r[15],
+                        fp.dt.eq(1 + ~read.dat_r[:15]),
+                    )],
+                "V1A": read_next(fp.v1[16:32], "V1B"),
+                "V1B": read_next(fp.v1[32:], "V2A"),
+                "V2A": read_next(fp.v2[:16], "V2B"),
+                "V2B": read_next(fp.v2[16:32], "V2C"),
+                "V2C": read_next(fp.v2[32:], "V3A") + short_send(2),
+                "V3A": read_next(fp.v3[:16], "V3B"),
+                "V3B": read_next(fp.v3[16:32], "V3C"),
+                "V3C": read_next(fp.v3[32:], "IDLE") + short_send(3) + [
+                    # 2-cycle delay, undo incr, will be redone in IDLE
+                    read.adr.eq(read.adr),
+                    If((read.adr < branch_start) |
+                       (read.adr >= branch_end),
+                        read.adr.eq(branch_start),
+                    )],
+                }
+        actions = dict((states[k], _) for k, _ in actions.items())
+        self.sync += Case(state, actions)
+       
 
 class DacOut(Module):
     def __init__(self):
         self.frame_in = Sink(frame_layout)
         frame = Record(frame_layout)
         self.frame = frame
+        self.busy = frame.dt > 0
 
         self.trigger = Signal()
-        t = Signal(15)
-        self.t = t
+        self.freerun = Signal()
         self.data = Signal(16)
 
-        self.freerun = Signal()
-        
-        self.busy = t < frame.dt
-
         self.sync += [
-                If(t < frame.dt,
-                    self.frame_in.ack.eq(0),
-                    self.data.eq(frame.v0[32:]),
+                If((frame.dt <= 2) &
+                        (self.freerun | self.trigger | ~frame.wait),
+                    self.frame_in.ack.eq(1),
+                ),
+                If(frame.dt > 1,
                     frame.v0.eq(frame.v0 + frame.v1),
                     frame.v1.eq(frame.v1 + frame.v2),
                     frame.v2.eq(frame.v2 + frame.v3),
-                    t.eq(t + 1),
+                    frame.dt.eq(frame.dt - 1),
+                ).Elif(self.frame_in.stb & self.frame_in.ack,
+                    self.frame_in.ack.eq(0),
+                    frame.eq(self.frame_in.payload),
                 ),
-                If((frame.dt == 0) | (t + 1 >= frame.dt),
-                    If(self.frame_in.stb &
-                        (self.freerun | self.trigger | ~frame.wait),
-                        self.frame_in.ack.eq(1),
-                        frame.raw_bits().eq(self.frame_in.payload.raw_bits()),
-                        t.eq(0),
-                    ),
-                ),
+                self.data.eq(frame.v0[32:]),
                 ]
-
 
 
 class Dac(Module):
@@ -197,12 +149,14 @@ class TB(Module):
 
 def main():
     from migen.sim.generic import Simulator, TopLevel
+    from migen.actorlib import sim
     from matplotlib import pyplot as plt
     import numpy as np
+    from scipy import interpolate
     import pdq
     pdq.Ftdi = pdq.FileFtdi
 
-    t = np.linspace(0, 3e-6, 11) + 12/50e6
+    t = np.arange(0, 6) * .25e-6
     v = [None] * 8
     v[1] = (1-np.cos(t/t[-1]*np.pi))/2
     p = pdq.Pdq("dac_test")
@@ -220,8 +174,12 @@ def main():
     sim.run(n)
     out = np.array(tb.outputs, np.uint16).view(np.int16)*20./(1<<16)
     tim = np.arange(out.shape[0])/50e6
-    plt.plot(t, v[1])
-    plt.plot(tim, out)
+    spline = interpolate.splrep(t, v[1], s=0, k=3)
+    tt = np.arange(t[0], t[-1], 1/p.freq)
+    vv = interpolate.splev(tt, spline, der=0)
+    plt.plot(t, v[1], "xk")
+    plt.plot(tt, vv, ",g")
+    plt.plot(tim-12/50e6, out, "-r")
     plt.show()
 
 
